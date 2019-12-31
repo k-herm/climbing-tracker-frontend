8b09940521ebd488cc950fbba189667a20f0483d
KHerman <kiesha.herman@gmail.com>
2019-12-31 10:39:47 -0800

    init project

---
+++ pages/_app.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import JssProvider from 'react-jss';
+import getPageContext from '../src/getPageContext';
+
+class MyApp extends App {
+  constructor() {
+    super();
+    this.pageContext = getPageContext();
+  }
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <Head>
+          <title>My page</title>
+        </Head>
+        {/* Wrap every page in Jss and Theme providers */}
+        {/* <JssProvider
+          registry={this.pageContext.sheetsRegistry}
+          generateClassName={this.pageContext.generateClassName}
+        > */}
+        {/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+        <ThemeProvider
+          theme={this.pageContext.theme}
+        // sheetsManager={this.pageContext.sheetsManager}
+        >
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          {/* Pass pageContext to the _document though the renderPage enhancer
+                to render collected styles on server-side. */}
+          <Component pageContext={this.pageContext} {...pageProps} />
+        </ThemeProvider>
+        {/* </JssProvider> */}
+      </>
+    );
+  }
+}
+
+export default MyApp;
