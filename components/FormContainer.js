e1855b7e59b464bc2bb2fb73e0a5e112a10837df
KHerman <kiesha.herman@gmail.com>
2020-02-15 17:44:05 -0800

    fix login form

--- components/FormContainer.js
+++ components/FormContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Paper, Grid, Button } from '@material-ui/core'
+import { Paper, Grid } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -8,42 +8,31 @@ const useStyles = makeStyles((theme) => ({
     '& h2': {
       color: theme.palette.primary.dark,
       textAlign: 'center'
+    },
+    '& button': {
+      padding: theme.spacing(2),
+      marginTop: theme.spacing(4),
+      width: '100%'
     }
-  },
-  button: {
-    padding: theme.spacing(2),
-    marginTop: theme.spacing(2),
   }
 }))
 
 const FormContainer = ({
   children,
   title,
-  method,
-  onSubmit,
-  submitTitle
 }) => {
   const classes = useStyles()
   return (
     <Paper className={classes.paper}>
-      <form method={method} onSubmit={onSubmit}>
-        <Grid
-          container
-          justify="center"
-          direction="column"
-          spacing={1}
-        >
-          <h2>{title}</h2>
-          {children}
-          <Button
-            className={classes.button}
-            variant="contained"
-            color="secondary"
-          >
-            {submitTitle}
-          </Button>
-        </Grid>
-      </form>
+      <Grid
+        container
+        justify="center"
+        direction="column"
+        spacing={1}
+      >
+        <h2>{title}</h2>
+        {children}
+      </Grid>
     </Paper>
   )
 }
--- components/user/login-form.js
+++ components/user/login-form.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
-import { TextField, Grid } from '@material-ui/core'
+import router from 'next/router'
+import { TextField, Grid, Button } from '@material-ui/core'
+import { UserContext } from '../../src/app/UserStore'
+
 import FormContainer from '../FormContainer'
+import { postRequest } from '../../src/request'
+import { getAPIBaseURL } from '../../config'
 
 const LoginForm = () => {
   const [state, setState] = useState({
@@ -15,32 +20,56 @@ const LoginForm = () => {
     })
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const url = `${getAPIBaseURL()}/login`
+      const response = await postRequest(url, {
+        email: state.email,
+        password: state.password
+      })
+      if (response.userId) {
+        router.push('/dashboard')
+      }
+      console.log(JSON.stringify(response, null, 2))
+    }
+    catch (error) {
+
+    }
+  }
+
   return (
-    <FormContainer
-      title="Login"
-      submitTitle="Continue"
-    >
-      <Grid item>
-        <TextField
-          required
-          name="email"
-          label="Email"
-          value={state.email}
-          onChange={handleChange}
-          fullWidth
-        />
-      </Grid>
-      <Grid item>
-        <TextField
-          required
-          name="password"
-          label="Password"
-          type="password"
-          value={state.password}
-          onChange={handleChange}
-          fullWidth
-        />
-      </Grid>
+    <FormContainer title="Login" >
+      <form method='post' onSubmit={onSubmit}>
+        <Grid item>
+          <TextField
+            required
+            name="email"
+            label="Email"
+            value={state.email}
+            onChange={handleChange}
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            required
+            name="password"
+            label="Password"
+            type="password"
+            value={state.password}
+            onChange={handleChange}
+            fullWidth
+          />
+        </Grid>
+        <Button
+          variant="contained"
+          color="secondary"
+          type="submit"
+        >
+          Continue
+        </Button>
+      </form>
     </FormContainer>
 
   )
