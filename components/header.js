1b33623d60fe7a88aa42541fe450e22cb5bd474f
KHerman <kiesha.herman@gmail.com>
2020-03-22 20:43:36 -0700

    move logout to appbar

--- components/header.js
+++ components/header.js
@@ -1,32 +1,85 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { useRouter } from 'next/router'
 import PropTypes from 'prop-types'
 import {
   AppBar,
-  Slide,
-  Typography,
-  useScrollTrigger
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography
 } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
 import { makeStyles } from '@material-ui/core/styles'
 
+import { postRequest } from '~/src/request'
+import { getAPIBaseURL } from '~/config'
+import { UserContext } from '~src/app/Contexts/UserStore'
+
+
 const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1)
+  },
   titleStyle: {
-    padding: theme.spacing(1),
-    marginLeft: theme.spacing(1)
+    padding: theme.spacing(1)
   }
 }))
 
 const Header = ({ title }) => {
-  const { titleStyle } = useStyles()
-  const trigger = useScrollTrigger()
+  const { container, titleStyle } = useStyles()
+  const [anchorE1, setAnchorE1] = useState(null)
+  const { setUser } = useContext(UserContext)
+  const router = useRouter()
+
+  const handleClick = (event) => setAnchorE1(event.currentTarget)
+  const handleClose = (event) => setAnchorE1(null)
+  const logout = async () => {
+    try {
+      handleClose()
+      const logoutUrl = `${getAPIBaseURL()}/logout`
+      await postRequest(logoutUrl)
+      setUser(null, null)
+      router.push('/login')
+    } catch (error) {
+      // TODO: display toast error message for logout
+      console.log(error)
+    }
+  }
+
   return (
-    // TODO - slider not working
-    <Slide appear={false} direction='down' in={!trigger}>
-      <AppBar position='static' color='secondary'>
+    <AppBar position='static' color='secondary'>
+      <Toolbar className={container}>
         <Typography variant='h5' className={titleStyle}>
           {title}
         </Typography>
-      </AppBar>
-    </Slide>
+        <IconButton
+          edge='end'
+          color='inherit'
+          aria-label='menu'
+          onClick={handleClick}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id='settings'
+          anchorEl={anchorE1}
+          anchorOrigin={{
+            vertical: 'bottom', horizontal: 'left'
+          }}
+          transformOrigin={{
+            vertical: 'top', horizontal: 'center'
+          }}
+          open={Boolean(anchorE1)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={logout}>Logout</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
   )
 }
 
--- components/nav.js
+++ components/nav.js
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react'
 import { useRouter } from 'next/router'
+import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import {
   BottomNavigation,
@@ -8,15 +8,11 @@ import {
   Fab
 } from '@material-ui/core'
 
-import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 import CreateIcon from '@material-ui/icons/Create'
 import FormatListBulletedRoundedIcon from '@material-ui/icons/FormatListBulletedRounded'
 import BarChartIcon from '@material-ui/icons/BarChart'
 import AddIcon from '@material-ui/icons/Add'
 
-import { postRequest } from '~/src/request'
-import { getAPIBaseURL } from '~/config'
-import { UserContext } from '~src/app/Contexts/UserStore'
 
 const useStyles = makeStyles((theme) => ({
   floatingIcon: {
@@ -34,36 +30,16 @@ const Nav = ({ redirect }) => {
   const { floatingIcon, nav } = useStyles()
 
   const router = useRouter()
-  const navItems = ['/login', '/dashboard', '/projects', '/journal']
+  const navItems = ['/dashboard', '/projects', '/journal']
 
   const getIndex = (path) => navItems.indexOf(path)
   const initialIndex = redirect ? getIndex(redirect) : getIndex(router.pathname)
 
   const [current, setCurrent] = useState(initialIndex);
-  const [shouldRoute, setShouldRoute] = useState(true)
-  const { setUser } = useContext(UserContext)
-
-  const logout = async () => {
-    try {
-      const logoutUrl = `${getAPIBaseURL()}/logout`
-      await postRequest(logoutUrl)
-      setUser(null, null)
-      setShouldRoute(true)
-    } catch (error) {
-      // TODO: display toast error message for logout
-      console.log(error)
-      setShouldRoute(false)
-    }
-  }
 
   const handleChange = async (event, newValue) => {
-    if (newValue === 0) {
-      await logout()
-    }
-    if (shouldRoute) {
-      setCurrent(newValue);
-      router.push(navItems[newValue])
-    }
+    setCurrent(newValue);
+    router.push(navItems[newValue])
   }
 
   return (
@@ -73,7 +49,6 @@ const Nav = ({ redirect }) => {
       showLabels
       className={nav}
     >
-      <BottomNavigationAction label="Logout" icon={<ExitToAppIcon />} />
       <BottomNavigationAction label="Dashboard" icon={<BarChartIcon />} />
       <BottomNavigationAction label="Projects" icon={<FormatListBulletedRoundedIcon />} />
       <BottomNavigationAction label="Journal" icon={<CreateIcon />} />
