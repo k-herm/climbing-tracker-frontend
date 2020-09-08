328b186ec4047f025e2b0294a0e2f78558608df8
KHerman <kiesha.herman@gmail.com>
2020-09-07 19:23:46 -0700

    update backend config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 export const devEndpoint = 'http://localhost:4000'
-export const prodEndpoint = 'http://localhost:4000'
-// export const prodEndpoint = 'https://api.iclimbtracker.com'
+export const prodEndpoint = 'https://api.iclimbtracker.com'
 
 export const getAPIBaseURL = () =>
   process.env.NODE_ENV === 'development' ? devEndpoint : prodEndpoint
